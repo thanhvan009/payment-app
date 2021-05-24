56476c56138f1a7894864915e07c25dedfd4268b
Vinh Nhan <nhan.tran@orientsoftware.com>
2021-05-24 14:07:17 +0700

    add navigation bar

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { StyledButton } from "./styled";
 
 const Button = () => {
-  return <StyledButton>
-    <p>HOME</p>
-    <p>SHOP</p>
-    <p>FEATURES</p>
-    <p>MEGA GROUP</p>
-    <p>ABOUT</p>
-    <p>BLOG</p>
-    <p>CONTACT</p>
-  </StyledButton>;
+  return (
+    <StyledButton>
+      <div className="menu">
+        <p>HOME</p>
+        <p>SHOP</p>
+        <p>FEATURES</p>
+        <p>MEGA GROUP</p>
+        <p>ABOUT</p>
+        <p>BLOG</p>
+        <p>CONTACT</p>
+        <div className="btn">
+          <p>Shopping Cart (0)</p>
+        </div>
+      </div>
+    </StyledButton>
+  );
 };
 
 export default Button;
--- src/components/Button/styled.tsx
+++ src/components/Button/styled.tsx
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.div`
-  display: flex;
-  background-color: black;
-  height: 50px;
+  div {
+    display: flex;
+    background-color: black;
+    height: 50px;
+    align-items: center;
+    justify-content: center;
+  }
 
   p {
     color: white;
+    padding: 0 20px;
+  }
+
+  p:hover {
+    cursor: pointer;
+    color: yellow;
+  }
+
+  .btn {
+    color: white;
+    background-color: red;
+    height: 100%;
+    width: 200px;
+    text-align: center;
   }
 `;
