2255536ac22ecb037c2bfb4e974baafb3d65a1b1
ManhHung020496 <hung.doan@orientsoftware.com>
2021-05-24 15:28:51 +0700

    update

--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -1,12 +1,14 @@
 import "./Slide_HomePage/styled";
 import SimpleSlider from "./Slide_HomePage/SlideView";
 import Button from "./../../components/Button/index";
+import CategoryBanner from "./../../containers/Homepage/Category_HomePage/Category";
 
 function HomePage() {
   return (
     <div>
       <Button />
       <SimpleSlider />
+      <CategoryBanner />
     </div>
   );
 }
