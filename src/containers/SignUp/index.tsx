7cb2197fc41efbd62218e2f9d99a0925ab170c1c
Van Duc Thanh <thanh.van@orientsoftware.com>
2021-06-01 15:28:28 +0700

    Update sign up layout

--- src/containers/SignUp/index.tsx
+++ src/containers/SignUp/index.tsx
@@ -1,62 +1,118 @@
-import { Form, Input, Button, Select } from 'antd';
+import {
+  Form, Input, Button, Select,
+  Radio, DatePicker, Checkbox
+} from 'antd';
+import { StyledFormSignUp } from './styled';
 
 const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
+  labelCol: { span: 6 },
+  wrapperCol: { span: 14 },
 };
 
-const { Option } = Select;
+const layoutSubmit = {
+  labelCol: { span: 0 },
+  wrapperCol: { span: 24 },
+};
+
 function SignUp() {
   return (
-    <div>
-      <h1>Register</h1>
+    <StyledFormSignUp>
+      <h3 className="main-title">Register</h3>
       <Form
         {...layout}
         name="register"
       >
-        <Form.Item
-          label="First name"
-          name="firstName"
-          rules={[{ required: true, message: 'Please input your first name!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Last name"
-          name="lastName"
-          rules={[{ required: true, message: 'Please input your last name!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="City"
-          name="city"
-          rules={
-            [
-              {
-                required: false,
-                message: 'Please input your city!'
-              }
-            ]
-          }
-        >
-          <Select defaultValue="">
-            <Option value="hcm">HCM</Option>
-            <Option value="hn">HN</Option>
-            <Option value="hue">Hue</Option>
-            <Option value="dn">DN</Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Sign up
-          </Button>
-        </Form.Item>
+        <div className="block-info">
+          <h4 className="subtitle">Your Personal Details</h4>
+          <div className="info-inner">
+            <Form.Item
+              label="Gender"
+              name="gender"
+            >
+              <Radio.Group>
+                <Radio value="Male">Male</Radio>
+                <Radio value="Female">Female</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item
+              label="First name"
+              name="firstName"
+              rules={[{required: true, message: 'Please input your first name!'}]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Last name"
+              name="lastName"
+              rules={[{required: true, message: 'Please input your last name!'}]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Date of birth"
+              name="dateOfBirth"
+            >
+              <DatePicker />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{required: true, message: 'Please input your email!'}]}
+            >
+              <Input />
+            </Form.Item>
+          </div>
+        </div>
+        <div className="block-info">
+          <h4 className="subtitle">Company Details</h4>
+          <div className="info-inner">
+            <Form.Item
+              label="Company name"
+              name="companyName"
+            >
+              <Input />
+            </Form.Item>
+          </div>
+        </div>
+        <div className="block-info">
+          <h4 className="subtitle">Options</h4>
+          <div className="info-inner">
+            <Form.Item
+              label="Newsletter"
+              name="newsletter"
+            >
+              <Checkbox />
+            </Form.Item>
+          </div>
+        </div>
+        <div className="block-info">
+          <h4 className="subtitle">Your password</h4>
+          <div className="info-inner">
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{required: true, message: 'Please input your password!'}]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              label="Confirm Password"
+              name="confirm password"
+              rules={[{required: true, message: 'Please input your confirm password!'}]}
+            >
+              <Input.Password />
+            </Form.Item>
+          </div>
+        </div>
+        <div className="submit-wrap text-center">
+          <Form.Item {...layoutSubmit}>
+            <Button type="primary" htmlType="submit">
+              Sign up
+            </Button>
+          </Form.Item>
+        </div>
       </Form>
-    </div>
+    </StyledFormSignUp>
   );
 }
 export default SignUp;
--- src/containers/SignUp/styled.tsx
+++ src/containers/SignUp/styled.tsx
@@ -1,4 +1,42 @@
 import styled from "styled-components";
 
-export const StyledSignUp = styled.div`
+export const StyledFormSignUp = styled.div`
+
+  .main-title,
+  .subtitle {
+    text-align: center;
+    border-bottom: 1px solid #ddd;
+    padding-bottom: 15px;
+    margin-bottom: 0;
+  }
+
+  .main-title {
+    font-size: 22px;
+    font-weight: bold;
+  }
+
+  .subtitle {
+    font-size: 16px;
+  }
+
+  .block-info {
+    margin-top: 20px;
+  }
+
+  .info-inner {
+    background-color: #eaeaea;
+    padding: 15px 0;
+    margin-bottom: 15px;
+  }
+
+  .submit-wrap {
+    padding-top: 30px;
+    button {
+      width: 130px;
+    }
+  }
+
+  .ant-picker {
+    width: 100%;
+  }
 `;
