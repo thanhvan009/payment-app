b9f9e716c6f2ca72ed31ddb5cefea6d1a306d946
Van Duc Thanh <thanh.van@orientsoftware.com>
2021-06-03 12:39:23 +0700

    Add mock data for product list'

---
+++ src/containers/Homepage/Product_HomePage/mockData.tsx
@@ -0,0 +1,50 @@
+export const productListMock = [
+  {
+    id: 1,
+    name: 'Coat',
+    price: '10$',
+    image: '/images/product_1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Dress',
+    price: '20$',
+    image: '/images/product_2.jpg'
+  },
+  {
+    id: 3,
+    name: 'Blouse',
+    price: '30$',
+    image: '/images/product_3.jpg'
+  },
+  {
+    id: 4,
+    name: 'Pants',
+    price: '40$',
+    image: '/images/product_4.jpg'
+  },
+  {
+    id: 5,
+    name: 'Shirt',
+    price: '50$',
+    image: '/images/product_1.jpg'
+  },
+  {
+    id: 6,
+    name: 'Shorts',
+    price: '60$',
+    image: '/images/product_2.jpg'
+  },
+  {
+    id: 7,
+    name: 'Suit',
+    price: '70$',
+    image: '/images/product_3.jpg'
+  },
+  {
+    id: 8,
+    name: 'Jacket',
+    price: '80$',
+    image: '/images/product_4.jpg'
+  }
+]
